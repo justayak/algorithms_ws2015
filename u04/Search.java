3c11e52eb355cf649a15131bb28aef3ed604536f
julian <jutanke@googlemail.com>
2015-11-10 10:54:25 +0100

    compares and debug info

--- u04/Search.java
+++ u04/Search.java
@@ -2,22 +2,42 @@ import java.math.*;
 
 public abstract class Search {
 
+    private int compares = 0;
+    private int last_n = 0;
+    private int last_found = -1;
+
     protected abstract int k(double a, int l, int r, double[] S);
 
     protected abstract int step(int n, int r, int l);
+
+    protected abstract String name();
+
+    @Override
+    public String toString() {
+        return this.name() + 
+            " compares: " + this.compares + 
+            " n:" + this.last_n + 
+            " found:" + this.last_found;
+    }
 
     /**
      *  @param S input list
      *  @param a searched value
      */
     public int search(double[] S, double a) {
-        return search(0, S.length - 1, S, a);
+        this.compares = 0;
+        this.last_n = S.length;
+        final int result = search(0, S.length - 1, S, a);
+        this.last_found = result;
+        return result;
     }
 
     private int search(int l, int r, double[] S, double a) {
         final int step = step(S.length, r, l);
         if (l <= r) {
             int k = k(a, l, r, S);
+            // 2 compares ( == and <)
+            this.compares += 2;
             if (is(S[k], a)) {
                 return k;
             } else if (S[k] > a) {
@@ -42,14 +62,17 @@ public abstract class Search {
      *  Example Binary Search
      */
     private static class BinarySearch extends Search {
-        
+       
         @Override
-        public int k(double a, int l, int r, double[] S) {
+        protected String name() { return "Binary Search";}
+
+        @Override
+        protected int k(double a, int l, int r, double[] S) {
             return (int) Math.ceil((l+r)/2.0f);
         }
 
         @Override
-        public int step(int n, int r, int l) {
+        protected int step(int n, int r, int l) {
            return 1;
         }
     }
@@ -58,6 +81,9 @@ public abstract class Search {
      *  Aufgabe 2.a
      */
     private static class InterpolationSearch extends BinarySearch {
+        
+        @Override
+        protected String name() { return "Interpolation Search";}
         
         @Override
         public int k(double a, int l, int r, double[] S) {
@@ -73,6 +99,9 @@ public abstract class Search {
     private static class QuadraticBinarySearch extends InterpolationSearch {
         
         @Override
+        protected String name() { return "Quadratic Search";}
+        
+        @Override
         public int step(int n, int r, int l) {
             final int m = r - l + 1;
             return (int) Math.ceil(Math.sqrt(m));
@@ -80,15 +109,19 @@ public abstract class Search {
     }
 
     public static void main(String[] args) {
-        final double[] S = {0, .01, .02, .3, .4, .5, .7, .75, .8, .81, .82, .9};
-        int pos = new BinarySearch().search(S, .4);
-        System.out.println("binary search: " + pos);
+        final BinarySearch bs = new BinarySearch();
+        final InterpolationSearch is = new InterpolationSearch();
+        final QuadraticBinarySearch qs = new QuadraticBinarySearch();
+
+        final double[] S = {0, .01, .02, .03, .04, .05, .07, .075, .08, .081, .2, .9};
+        int pos = bs.search(S, .2);
+        System.out.println(bs);
         
-        pos = new InterpolationSearch().search(S, .4);
-        System.out.println("Interpolation search: " + pos);
+        pos = is.search(S, .2);
+        System.out.println(is);
         
-        pos = new QuadraticBinarySearch().search(S, .4);
-        System.out.println("Quadratic search: " + pos);
+        pos = qs.search(S, .2);
+        System.out.println(is);
     }
 
 }
